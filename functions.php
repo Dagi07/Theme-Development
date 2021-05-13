44dcfc7263186edc1be037c6a81c7de16c46ad5b
D. Amanuel <dagmawiamanuel.com>
2021-05-14 00:52:44 +0300

    Basic setup of the theme, enqueue/load stylesheets through functions.php, load scripts, javascript and jquery through functions.php as well.

---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+
+function load_stylesheets()
+{
+
+    wp_register_style('bootstrap', get_template_directory_uri(). '/assets/css/bootstrap.min.css',
+    array(), false, all);
+    wp_enqueue_style('bootstrap');
+
+    wp_register_style('style', get_template_directory_uri(). '/style.css',
+    array(), false, all);
+    wp_enqueue_style('style');
+
+}
+add_action('wp_enqueue_scripts', load_stylesheets);
+
+function include_jquery()
+{
+
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery-3.6.0.min.js', '', 1, true);
+
+    add_action('wp_enqueue_scripts', 'jquery');
+}
+add_action('wp_enqueue_scripts', 'include_jquery');
+
+
+function loadjs()
+{
+    wp_register_script('customjs', get_template_directory_uri() . '/assets/js/scripts.js', '', 1, true);
+    wp_enqueue_script('customjs');
+}
+add_action('wp_enqueue_scripts', 'loadjs');
